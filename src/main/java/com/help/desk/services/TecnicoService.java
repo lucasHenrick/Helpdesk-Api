7a33e9c4f2c0c700bd98620acfebd4c7bc4a0784
Lucas Henrick <lucashenrikc@gmail.com>
2022-06-04 11:53:54 -0300

    Aula 13 - Adição do método findAll para retornar todos os tecnicos.

--- src/main/java/com/help/desk/services/TecnicoService.java
+++ src/main/java/com/help/desk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.help.desk.domain.Tecnico;
 import com.help.desk.repository.TecnicoRepository;
+import java.util.List;
 import com.help.desk.services.exception.ObjectnotFoundException;
 
 @Service
@@ -19,5 +20,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto com esse Id não encontrado"));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 }
