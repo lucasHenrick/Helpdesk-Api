eb69877c62a6f92b5a9bb10d5b62da4f2029fcb7
Lucas Henrick <lucashenrikc@gmail.com>
2022-06-04 11:15:44 -0300

    Aula 11 - Adição da classe DTO

--- src/main/java/com/help/desk/resources/TecnicoResource.java
+++ src/main/java/com/help/desk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.help.desk.domain.Tecnico;
+import com.help.desk.domain.dtos.TecnicoDTO;
 import com.help.desk.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findByiD(@PathVariable Integer id ) {
+	public ResponseEntity<TecnicoDTO> findByiD(@PathVariable Integer id ) {
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 
 }
