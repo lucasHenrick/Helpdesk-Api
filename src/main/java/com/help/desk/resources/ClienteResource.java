e235fe97653efbe2d9c61b625b2ad104a48e7235
Lucas Henrick <lucashenrikc@gmail.com>
2022-06-06 20:13:58 -0300

    Aula 19 - Criações da classes DTO, Resource e Service referente a Cliente.

---
+++ src/main/java/com/help/desk/resources/ClienteResource.java
@@ -0,0 +1,66 @@
+package com.help.desk.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.help.desk.domain.Cliente;
+import com.help.desk.domain.dtos.ClienteDTO;
+import com.help.desk.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClienteDTO> findByiD(@PathVariable Integer id ) {
+		Cliente obj = service.findById(id);
+		return ResponseEntity.ok().body(new ClienteDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> 
+		new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClienteDTO> create(@Valid  @RequestBody ClienteDTO ObjDTO) {
+		Cliente newObj = service.create(ObjDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClienteDTO> update( @PathVariable Integer id, @Valid @RequestBody ClienteDTO objDTO ) {
+		Cliente obj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new ClienteDTO(obj));
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClienteDTO> delete(@PathVariable Integer id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+
+}
---
+++ src/main/java/com/help/desk/services/ClienteService.java
@@ -0,0 +1,77 @@
+package com.help.desk.services;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.help.desk.domain.Cliente;
+import com.help.desk.domain.Pessoa;
+import com.help.desk.domain.dtos.ClienteDTO;
+import com.help.desk.repository.ClienteRepository;
+import com.help.desk.repository.PessoaRepository;
+import java.util.List;
+
+import com.help.desk.services.exception.DataIntegrityVioolationException;
+import com.help.desk.services.exception.ObjectnotFoundException;
+
+@Service
+public class ClienteService {
+
+	
+	@Autowired
+	private ClienteRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto com esse Id não encontrado"));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+	
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+	}
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityVioolationException("Cliente Possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+	}
+	private void validaPorCpfEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());		
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityVioolationException("CPF já Cadastrado no sistema");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId() ) {
+			throw new DataIntegrityVioolationException("E-mail já Cadastrado no sistema");
+		}
+				
+	}
+
+
+	
+}
+
+
