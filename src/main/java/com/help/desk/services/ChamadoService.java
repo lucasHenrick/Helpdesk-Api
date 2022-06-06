f0bf93afe7a18c33784a8e0f03fd9c39860a2994
Lucas Henrick <lucashenrikc@gmail.com>
2022-06-06 20:56:53 -0300

    Aula 22 - Adição dó método create para chamado

--- src/main/java/com/help/desk/services/ChamadoService.java
+++ src/main/java/com/help/desk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.help.desk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.help.desk.domain.Chamado;
+import com.help.desk.domain.Cliente;
+import com.help.desk.domain.Tecnico;
+import com.help.desk.domain.dtos.ChamadoDTO;
+import com.help.desk.enums.Prioridade;
+import com.help.desk.enums.Status;
 import com.help.desk.repository.ChamadoRepository;
 import com.help.desk.services.exception.ObjectnotFoundException;
 
@@ -16,7 +23,10 @@ public class ChamadoService {
 	
 	@Autowired
 	private ChamadoRepository repository;
-	
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -26,5 +36,30 @@ public class ChamadoService {
 	
 	public List<Chamado> findByAll() {
 		return repository.findAll();
+	}
+
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		
+		return chamado;
 	}
 }
